c37e3a88c2a9005f3392849051a50361c230b8c2
Kostas Cheliotis <cheliotis.k@gmail.com>
2022-09-11 18:30:49 +0300

    openRouteService - WIP

---
+++ AR-Navigation/Assets/Scripts/Auxiliary/OpenRouteServiceResponse.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Auxiliary
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Engine
+    {
+        public string version { get; set; }
+        public DateTime build_date { get; set; }
+        public DateTime graph_date { get; set; }
+    }
+
+    public class Feature
+    {
+        public List<double> bbox { get; set; }
+        public string type { get; set; }
+        public Properties properties { get; set; }
+        public Geometry geometry { get; set; }
+    }
+
+    public class Geometry
+    {
+        public List<List<double>> coordinates { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string attribution { get; set; }
+        public string service { get; set; }
+        public long timestamp { get; set; }
+        public Query query { get; set; }
+        public Engine engine { get; set; }
+    }
+
+    public class Properties
+    {
+        public List<Segment> segments { get; set; }
+        public Summary summary { get; set; }
+        public List<int> way_points { get; set; }
+    }
+
+    public class Query
+    {
+        public List<List<double>> coordinates { get; set; }
+        public string profile { get; set; }
+        public string format { get; set; }
+    }
+
+    public class OpenRouteServiceResponse
+    {
+        public string type { get; set; }
+        public List<Feature> features { get; set; }
+        public List<double> bbox { get; set; }
+        public Metadata metadata { get; set; }
+    }
+
+    public class Segment
+    {
+        public double distance { get; set; }
+        public double duration { get; set; }
+        public List<Step> steps { get; set; }
+    }
+
+    public class Step
+    {
+        public double distance { get; set; }
+        public double duration { get; set; }
+        public int type { get; set; }
+        public string instruction { get; set; }
+        public string name { get; set; }
+        public List<int> way_points { get; set; }
+    }
+
+    public class Summary
+    {
+        public double distance { get; set; }
+        public double duration { get; set; }
+    }
+
+
+}
---
+++ AR-Navigation/Assets/Scripts/RoutingHandler.cs
@@ -0,0 +1,48 @@
+﻿using Assets.Scripts.Auxiliary;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Net.Http;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RoutingHandler : MonoBehaviour
+    {
+        private string directionsApiBase = "https://api.openrouteservice.org/v2/directions";
+        private string directionsProfile = "foot-walking";
+        [SerializeField] private Vector2 startLatLong;
+        [SerializeField] private Vector2 endLatLong;
+
+        [SerializeField] APIKeyContainer apiKeyContainer;
+
+        [ContextMenu("RunQuery")]
+        public void StartQuery()
+        {
+            RouteDirections_GET();
+        }
+
+        public async void RouteDirections_GET()
+        {
+            if (apiKeyContainer == null)
+                return;
+
+            var baseAddress = new Uri($"{directionsApiBase}/{directionsProfile}?api_key={apiKeyContainer.OpenRouteServiceApiKey}&start={startLatLong.y},{startLatLong.x}&end={endLatLong.y},{endLatLong.x}");
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8");
+
+                using (var response = await httpClient.GetAsync(baseAddress))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject(responseData);
+
+                    OpenRouteServiceResponse data2 = JsonConvert.DeserializeObject<OpenRouteServiceResponse>(responseData);
+                    var bob = data2?.type;
+                }
+            }
+        }
+    }
+}
+
