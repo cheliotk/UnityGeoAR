9cfa56ebcac5e265721c50a6ef720440f14296c4
Kostas Cheliotis <cheliotis.k@gmail.com>
2022-09-12 16:46:09 +0300

    added geocoder input field and handler

---
+++ AR-Navigation/Assets/Scripts/GeocoderInputHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GeocoderInputHandler : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField inputField;
+
+        private Geocoder geocoder;
+
+        private void Start()
+        {
+            geocoder = Geocoder.Instance;
+
+            if(inputField != null)
+                inputField.onSubmit.AddListener(MakeQuery);
+        }
+
+        private void OnDestroy()
+        {
+            if (inputField != null)
+                inputField.onSubmit.RemoveListener(MakeQuery);
+        }
+
+        public void MakeQueryWithCurrentString()
+        {
+            if (string.IsNullOrWhiteSpace(inputField?.text))
+                return;
+
+            MakeQuery(inputField.text);
+        }
+
+        public void MakeQuery(string queryText)
+        {
+            geocoder.MakeQuery(queryText);
+        }
+    }
+}
