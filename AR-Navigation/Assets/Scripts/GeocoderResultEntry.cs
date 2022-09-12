1b8add3de8a284c03cb199086f5cac7616856a21
Kostas Cheliotis <cheliotis.k@gmail.com>
2022-09-12 17:15:00 +0300

    added geocoder result visualization

---
+++ AR-Navigation/Assets/Scripts/GeocoderResultEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GeocoderResultEntry : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text label;
+
+        public void SetLabel(string labelText)
+        {
+            label.text = labelText;
+        }
+    }
+}
---
+++ AR-Navigation/Assets/Scripts/GeocoderResultsVisualizer.cs
@@ -0,0 +1,48 @@
+﻿using Assets.Scripts.Auxiliary.Nominatim;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GeocoderResultsVisualizer : MonoBehaviour
+    {
+        [SerializeField] private GameObject resultPrefab;
+        [SerializeField] private Transform container;
+
+        private Geocoder geocoder;
+
+        private void Start()
+        {
+            geocoder = Geocoder.Instance;
+            if(geocoder != null)
+                geocoder.onGeocodeResultsReceived += Geocoder_onGeocodeResultsReceived;
+        }
+
+        private void OnDestroy()
+        {
+            if(geocoder != null)
+                geocoder.onGeocodeResultsReceived -= Geocoder_onGeocodeResultsReceived;
+        }
+
+        private void Geocoder_onGeocodeResultsReceived(object sender, NominatimResponse e)
+        {
+            ClearChildren();
+            foreach (Feature feature in e.features)
+            {
+                GameObject result = Instantiate(resultPrefab, container);
+                GeocoderResultEntry entry = result.GetComponent<GeocoderResultEntry>();
+                entry.SetLabel(feature.properties.display_name);
+            }
+        }
+
+        private void ClearChildren()
+        {
+            for (int i = 0; i < container.childCount; i++)
+            {
+                Transform child = container.GetChild(i);
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
