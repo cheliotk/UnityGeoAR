9a11607c405c2396b6e49f1c8fb3b4cc94c2ec6a
Kostas Cheliotis <cheliotis.k@gmail.com>
2023-11-06 20:46:10 +0200

    WorldToUnityService wip

---
+++ AR-Navigation/Assets/Scripts/Services/WorldToUnityService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Services
+{
+    public class WorldToUnityService
+    {
+        private ReprojectionService reprojectionService;
+        private ElevationQueryService elevationQueryService;
+        private Vector2 originLocationSourceCRS;
+        private Vector2 originLocationDestinationCRS;
+
+        public WorldToUnityService(ReprojectionService reprojectionService, ElevationQueryService elevationQueryService, Vector2 locationSourceLatLong)
+        {
+            this.reprojectionService = reprojectionService;
+            this.elevationQueryService = elevationQueryService;
+            originLocationSourceCRS = locationSourceLatLong;
+            originLocationDestinationCRS = reprojectionService.ReprojectPoint(originLocationSourceCRS.x, originLocationSourceCRS.y);
+        }
+    }
+}
